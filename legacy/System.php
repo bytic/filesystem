5f94aaeb89ac2c764b5a2553a8df168b19c9a8a2
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-12-14 06:30:00 +0000

    Apply fixes from StyleCI

--- legacy/System.php
+++ legacy/System.php
@@ -7,7 +7,6 @@ use Nip\Filesystem\Exception\IOException;
  */
 class Nip_File_System
 {
-
     protected $_uploadErrors = [
         0 => 'There is no error, the file uploaded with success',
         1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
